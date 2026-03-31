6ba413a97c89de4d314602a1400308771e9c6e12
Vincentius <vincentius85@gmail.com>
2026-04-01 02:36:43 +0700

    feat: adding routes for auth, admin, and main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import "dotenv/config"
 import express from "express"
 import { constants } from "node:http2"
-import usersRouter from "./routes/users.router.js"
+import authRouter from "./routes/auth.router.js"
+import adminRouter from "./routes/admin.router.js"
+import mainRouter from "./routes/main.router.js"
 
 const app = express()
 const PORT = process.env.PORT || 8888
@@ -15,7 +16,9 @@ app.get("/", function (req, res) {
   })
 })
 
-app.use("/users", usersRouter)
+app.use("/", mainRouter)
+app.use("/auth", authRouter)
+app.use("/admin", adminRouter)
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}`)
