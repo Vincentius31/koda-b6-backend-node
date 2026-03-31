7240c02eef80be479be91b35c327ab9c49ff57bc
Vincentius <vincentius85@gmail.com>
2026-04-01 02:31:25 +0700

    feat: adding and using the / for mainRouter

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import express from "express"
 import { constants } from "node:http2"
 import authRouter from "./routes/auth.router.js"
 import adminRouter from "./routes/admin.router.js"
+import mainRouter from "./routes/main.router.js"
 
 const app = express()
 const PORT = process.env.PORT || 8888
@@ -17,6 +18,7 @@ app.get("/", function (req, res) {
 
 app.use("/auth", authRouter)
 app.use("/admin", adminRouter)
+app.use("/", mainRouter)
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}`)
