9397f116d31775365f6fed1766f1e217207b9136
Vincentius <vincentius85@gmail.com>
2026-04-01 02:50:42 +0700

    feat: adding endpoint reviews for the landing page

--- src/routes/main.router.js
+++ src/routes/main.router.js
@@ -4,5 +4,6 @@ import * as landingController from "../controllers/landing.controller.js"
 const mainRouter = Router()
 
 mainRouter.get("/landing/recommended-products", landingController.getRecommendedProducts)
+mainRouter.get("/landing/reviews", landingController.getLatestReviews)
 
 export default mainRouter
