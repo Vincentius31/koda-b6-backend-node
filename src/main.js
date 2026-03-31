38e328c70759acc85ef826cc9e01b43a63fdbdd8
Vincentius <vincentius85@gmail.com>
2026-04-01 02:51:06 +0700

    fix: fixing the merge issues

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import express from "express"
+import { constants } from "node:http2"
+import authRouter from "./routes/auth.router.js"
+import adminRouter from "./routes/admin.router.js"
+import mainRouter from "./routes/main.router.js"
+
+const app = express()
+const PORT = process.env.PORT || 8888
+
+app.use(express.json())
+
+app.get("/", function (req, res) {
+  res.status(constants.HTTP_STATUS_OK).json({
+    success: true,
+    message: "Backend is running well!"
+  })
+})
+
+app.use("/", mainRouter)
+app.use("/auth", authRouter)
+app.use("/admin", adminRouter)
+
+app.listen(PORT, function () {
+  console.log(`App listening on port ${PORT}`)
+})
