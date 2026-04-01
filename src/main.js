31d10e822b02bd74b6e6e754286bfc1e4ee4a41a
Vincentius <vincentius85@gmail.com>
2026-04-02 03:07:08 +0700

    fix: fixing the endpoint

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import authRouter from "./routes/auth.router.js"
 import adminRouter from "./routes/admin.router.js"
 import mainRouter from "./routes/main.router.js"
 import docsRouter from "./routes/docs.js"
+import usersRouter from "./routes/users.router.js"
 
 const app = express()
 const PORT = process.env.PORT || 8888
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use("/", mainRouter)
 app.use("/auth", authRouter)
 app.use("/admin", adminRouter)
+app.use("/", usersRouter)
 app.use("/docs", docsRouter)
 
 app.listen(PORT, function () {
