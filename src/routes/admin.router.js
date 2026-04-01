5d74f1b1e0e27bea4dc16fbad171b974b746f4e4
Vincentius <vincentius85@gmail.com>
2026-04-01 13:28:58 +0700

    feat: adding auth middleware for admin routes

--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -1,7 +1,10 @@
 import { Router } from "express"
 import userRouter from "./users.router.js"
+import auth from "./middlewares/auth.middleware.js"
 
 const adminRouter = Router()
+
+adminRouter.use(auth)
 
 adminRouter.use("/users", userRouter)
 
