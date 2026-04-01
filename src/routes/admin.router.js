fa89db886eae637cc0558b10b7374e47d4dc1196
Vincentius <vincentius85@gmail.com>
2026-04-02 03:06:20 +0700

    fix: moving the CRUD for users in admin and now only in admin

--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -1,11 +1,15 @@
 import { Router } from "express"
-import userRouter from "./users.router.js"
+import * as userController from "../controllers/users.controller.js"
 import auth from "../middlewares/auth.middleware.js"
 
 const adminRouter = Router()
 
 adminRouter.use(auth)
 
-adminRouter.use("/users", userRouter)
+adminRouter.get("/users", userController.getAllUsers)
+adminRouter.get("/users/:id", userController.getUserById)
+adminRouter.post("/users", userController.createUser)
+adminRouter.patch("/users/:id", userController.updateUser)
+adminRouter.delete("/users/:id", userController.deleteUser)
 
 export default adminRouter
