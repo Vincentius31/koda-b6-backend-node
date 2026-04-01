4157efc973c09553dc737073bf5a379cca8fb7d4
Vincentius <vincentius85@gmail.com>
2026-04-01 14:46:21 +0700

    feat: adding endpoint to use swagger docs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import express from "express"
-import { constants } from "node:http2"
 import authRouter from "./routes/auth.router.js"
 import adminRouter from "./routes/admin.router.js"
 import mainRouter from "./routes/main.router.js"
+import docsRouter from "./routes/docs.js"
 
 const app = express()
 const PORT = process.env.PORT || 8888
@@ -12,6 +12,7 @@ app.use(express.json())
 app.use("/", mainRouter)
 app.use("/auth", authRouter)
 app.use("/admin", adminRouter)
+app.use("/docs", docsRouter)
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}`)
