1b846cbf9d7847e1ace0cf637653462cf21050e4
Vincentius <vincentius85@gmail.com>
2026-04-02 09:27:43 +0700

    feat: adding route for the admin CRUD for the transaction and transaction products

--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import * as userController from "../controllers/users.controller.js"
+import * as transactionController from "../controllers/transaction.controller.js"
 import auth from "../middlewares/auth.middleware.js"
 
 const adminRouter = Router()
@@ -11,5 +12,17 @@ adminRouter.get("/users/:id", userController.getUserById)
 adminRouter.post("/users", userController.createUser)
 adminRouter.patch("/users/:id", userController.updateUser)
 adminRouter.delete("/users/:id", userController.deleteUser)
+
+adminRouter.get("/transaction", transactionController.getAllTransactions)
+adminRouter.get("/transaction/:id", transactionController.getTransactionById)
+adminRouter.post("/transaction", transactionController.createTransaction)
+adminRouter.patch("/transaction/:id", transactionController.updateTransaction)
+adminRouter.delete("/transaction/:id", transactionController.deleteTransaction)
+
+adminRouter.get("/transactionproduct", transactionController.getAllTransactionProducts)
+adminRouter.get("/transactionproduct/:id", transactionController.getTransactionProductById)
+adminRouter.post("/transactionproduct", transactionController.createTransactionProduct)
+adminRouter.patch("/transactionproduct/:id", transactionController.updateTransactionProduct)
+adminRouter.delete("/transactionproduct/:id", transactionController.deleteTransactionProduct)
 
 export default adminRouter
