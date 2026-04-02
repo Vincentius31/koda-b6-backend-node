1ddb401890e0baf6bddc6db378e7353cb7869c8a
Vincentius <vincentius85@gmail.com>
2026-04-02 09:28:25 +0700

    feat: adding endpoint for the users to checkout product, get transaction, and get detail transaction

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import * as cartController from "../controllers/cart.controller.js"
+import * as transactionController from "../controllers/transaction.controller.js"
 import auth from "../middlewares/auth.middleware.js"
 
 const userRouter = Router()
@@ -10,5 +11,9 @@ userRouter.get("/cart", cartController.getUserCart)
 userRouter.post("/cart", cartController.createCart)
 userRouter.patch("/cart/:id", cartController.updateCart)
 userRouter.delete("/cart/:id", cartController.deleteCart)
+
+userRouter.post("/checkout", transactionController.checkout)
+userRouter.get("/history", transactionController.getTransactionHistory)
+userRouter.get("/history/:id", transactionController.getTransactionDetail)
 
 export default userRouter
