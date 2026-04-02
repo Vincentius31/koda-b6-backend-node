72e9e7d53d0e9fa2a768459c91fc23fcdb080769
Vincentius <vincentius85@gmail.com>
2026-04-02 09:50:28 +0700

    feat: adding new routes for the forgot password

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -5,5 +5,7 @@ const authRouter = Router()
 
 authRouter.post("/register", authController.register)
 authRouter.post("/login", authController.login)
+authRouter.post("/forgot-password", authController.requestForgotPassword)
+authRouter.patch("/forgot-password", authController.resetPassword)
 
 export default authRouter
