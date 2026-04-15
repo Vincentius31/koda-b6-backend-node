320ed57e598998438fb6cea327df44885c2d5062
Vincentius <vincentius85@gmail.com>
2026-04-15 10:40:45 +0700

    feat: adding file uploads for save the image from profile, products, and user admin

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 8888
 
 app.use(express.json())
 
+app.use("/uploads", express.static("uploads"))
+
 app.use("/", mainRouter)
 app.use("/auth", authRouter)
 app.use("/admin", adminRouter)
