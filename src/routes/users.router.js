2f0fc2edc8e2e8c660a8ffcd926d561d7edcfe82
Vincentius <vincentius85@gmail.com>
2026-04-15 10:39:05 +0700

    feat: adding endpoint for the profile page endpoint

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,11 +1,17 @@
 import { Router } from "express"
 import * as cartController from "../controllers/cart.controller.js"
 import * as transactionController from "../controllers/transaction.controller.js"
+import * as userController from "../controllers/users.controller.js"
 import auth from "../middlewares/auth.middleware.js"
+import uploadMiddleware, { uploadProfileMiddleware } from "../middlewares/upload,middleware.js"
 
 const userRouter = Router()
 
 userRouter.use(auth)
+
+userRouter.get("/profile", userController.getProfile)
+userRouter.patch("/profile", userController.updateProfile)
+userRouter.post("/profile/upload", uploadProfileMiddleware("uploads/users").single("profile_image"), userController.uploadProfilePicture)
 
 userRouter.get("/cart", cartController.getUserCart)
 userRouter.post("/cart", cartController.createCart)
@@ -13,7 +19,7 @@ userRouter.patch("/cart/:id", cartController.updateCart)
 userRouter.delete("/cart/:id", cartController.deleteCart)
 
 userRouter.post("/checkout", transactionController.checkout)
-userRouter.get("/history", transactionController.getTransactionHistory)
-userRouter.get("/history/:id", transactionController.getTransactionDetail)
+userRouter.get("/transactions", transactionController.getTransactionHistory)
+userRouter.get("/transactions/:id", transactionController.getTransactionDetail)
 
 export default userRouter
