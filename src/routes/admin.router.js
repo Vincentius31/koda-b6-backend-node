80daa97cc1efdac715746d69732b989ccf6a7aeb
Vincentius <vincentius85@gmail.com>
2026-04-15 10:39:48 +0700

    feat: adding endpoint for dashboard admin and product admin CRUD

--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -1,17 +1,33 @@
 import { Router } from "express"
 import * as userController from "../controllers/users.controller.js"
 import * as transactionController from "../controllers/transaction.controller.js"
+import * as dashboardController from "../controllers/dashboard.controller.js"
+import * as adminProductController from "../controllers/admin-product.controller.js"
 import auth from "../middlewares/auth.middleware.js"
+import uploadMiddleware, { uploadProfileMiddleware } from "../middlewares/upload,middleware.js"
 
 const adminRouter = Router()
 
 adminRouter.use(auth)
 
+adminRouter.get("/dashboard/sales-category", dashboardController.getSalesCategory)
+adminRouter.get("/dashboard/best-sellers", dashboardController.getBestSellers)
+adminRouter.get("/dashboard/order-stats", dashboardController.getOrderStats)
+
 adminRouter.get("/users", userController.getAllUsers)
 adminRouter.get("/users/:id", userController.getUserById)
 adminRouter.post("/users", userController.createUser)
 adminRouter.patch("/users/:id", userController.updateUser)
+adminRouter.patch("/users/:id/upload", uploadProfileMiddleware("uploads/users").single("profile_image"), userController.uploadUserProfile)
 adminRouter.delete("/users/:id", userController.deleteUser)
+
+adminRouter.get("/product/promos", adminProductController.getPromos)
+adminRouter.get("/product", adminProductController.getAllProducts)
+adminRouter.get("/product/:id", adminProductController.getProductById)
+adminRouter.post("/product", adminProductController.createProduct)
+adminRouter.patch("/product/:id", adminProductController.updateProduct)
+adminRouter.patch("/product/:id/images", uploadMiddleware("uploads/products").array("images"), adminProductController.uploadImages)
+adminRouter.delete("/product/:id", adminProductController.deleteProduct)
 
 adminRouter.get("/transaction", transactionController.getAllTransactions)
 adminRouter.get("/transaction/:id", transactionController.getTransactionById)
